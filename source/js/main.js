3debd3451732a242d8185be0ecd4f6f7fd71fe8a
Rinat Atnashev <atnashev@mail.ru>
2018-02-16 14:14:34 +0300

    Изменил состояние карты follback

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,10 @@
 var navMain = document.querySelector('.main-nav');
+var contactMap = document.querySelector('.contacts__map');
 var navToggle = document.querySelector('.main-nav__toggle');
 var navToggleClose = document.querySelector('.main-nav__toggle-close');
 
 navMain.classList.remove('main-nav--nojs');
+contactMap.classList.remove('contacts__map--nojs')
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--opened')) {
